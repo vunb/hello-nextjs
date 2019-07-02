5ce8035cc708f1c2bdefb71098c8435947a7a099
Vunb <nhubaovu@gmail.com>
2019-07-02 18:31:14 +0700

    minor fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const expess = require('express')
 const next = require('next')
 
-const dev = process.env !== 'production'
+const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 
 const handle = app.getRequestHandler();
@@ -9,7 +9,14 @@ const handle = app.getRequestHandler();
 app.prepare()
 .then(() => {
     const server = expess();
-    server.get('*', (req, res) => handle(req, res));
+    server.get('/p/:id', (req, res) => {
+        const actualPage = '/post';
+        const queryParams = { title: req.params.id };
+        app.render(req, res, actualPage, queryParams);
+    });
+    server.get('*', (req, res) => {
+        return handle(req, res);
+    });
     server.listen(3000, err => {
         if(err) throw err;
         console.log('> Ready on http://localhost:3000')
